7df41510a71e09459496482c8c009eec0e484833
Pranay Buradkar <pranayburadkar05@gmail.com>
2025-06-07 12:04:41 +0530

    car store

--- store/engine/engine.go
+++ store/engine/engine.go
@@ -1,1 +1,33 @@
 package engine
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/pranayyb/DriveThrough/models"
+)
+
+type EngineStore struct {
+	db *sql.DB
+}
+
+func New(db *sql.DB) *EngineStore {
+	return &EngineStore{
+		db: db,
+	}
+}
+
+func (e EngineStore) GetEngineById(ctx context.Context, id string) (models.Engine, error) {
+
+}
+
+func (e EngineStore) CreateEngine(ctx context.Context, engineReq *models.EngineRequest) (models.Engine, error) {
+
+}
+
+func (e EngineStore) UpdateEngine(ctx context.Context, id string, engine *models.EngineRequest) (models.Engine, error) {
+
+}
+func (e EngineStore) DeleteEngine(ctx context.Context, id string) (models.Engine, error) {
+
+}
