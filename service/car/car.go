cac2a946a4792cb5f4046a7f9c0feaa860f85882
Pranay Buradkar <pranayburadkar05@gmail.com>
2025-06-08 12:15:24 +0530

    car service layer

--- service/car/car.go
+++ service/car/car.go
@@ -1,1 +1,63 @@
 package car
+
+import (
+	"context"
+	"github.com/pranayyb/DriveThrough/models"
+	"github.com/pranayyb/DriveThrough/store"
+)
+
+type CarService struct {
+	store store.CarStoreInterface
+}
+
+func NewCarService(store store.CarStoreInterface) *CarService {
+	return &CarService{
+		store: store,
+	}
+}
+
+func (s *CarService) GetCarById(id string, ctx context.Context) (*models.Car, error) {
+	car, err := s.store.GetCarById(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	return &car, nil
+}
+
+func (s *CarService) GetCarByBrand(brand string, ctx context.Context, isEngine bool) ([]models.Car, error) {
+	car, err := s.store.GetCarByBrand(ctx, brand, isEngine)
+	if err != nil {
+		return nil, err
+	}
+	return car, nil
+}
+
+func (s *CarService) CreateCar(carReq *models.CarRequest, ctx context.Context) (*models.Car, error) {
+	if err := models.ValidateRequest(*carReq); err != nil {
+		return nil, err
+	}
+	car, err := s.store.CreateCar(ctx, carReq)
+	if err != nil {
+		return nil, err
+	}
+	return &car, nil
+}
+
+func (s *CarService) UpdateCar(id string, carReq *models.CarRequest, ctx context.Context) (*models.Car, error) {
+	if err := models.ValidateRequest(*carReq); err != nil {
+		return nil, err
+	}
+	car, err := s.store.UpdateCar(ctx, id, carReq)
+	if err != nil {
+		return nil, err
+	}
+	return &car, nil
+}
+
+func (s *CarService) DeleteCar(id string, ctx context.Context) (*models.Car, error) {
+	car, err := s.store.DeleteCar(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	return &car, nil
+}
--- store/interface.go
+++ store/interface.go
@@ -1,1 +1,15 @@
 package store
+
+import (
+	"context"
+
+	"github.com/pranayyb/DriveThrough/models"
+)
+
+type CarStoreInterface interface {
+	GetCarById(ctx context.Context, id string) (models.Car, error)
+	GetCarByBrand(ctx context.Context, brand string, isEngine bool) ([]models.Car, error)
+	CreateCar(ctx context.Context, carReq *models.CarRequest) (models.Car, error)
+	UpdateCar(ctx context.Context, id string, carReq *models.CarRequest) (models.Car, error)
+	DeleteCar(ctx context.Context, id string) (models.Car, error)
+}
